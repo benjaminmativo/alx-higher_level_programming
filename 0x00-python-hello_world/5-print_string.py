b2e791e572dd8fccf234764ab81e90a7ab642410
benjaminmativo <benjaminmativomusau@gmail.com>
2022-11-28 21:16:08 -0800

    hello world

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-_str = str[0:9]
-print('{:s}{:s}{:s}\n{:s}'.format(str, str, str, _str))
+print("{:s}{:s}{:s}".format(str, str, str))
+print("{:.9s}".format(str))
